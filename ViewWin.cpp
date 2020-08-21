961d68e38ec30265aa1a058a22f35fcb081a4ffe
fire-eggs <lifeattickville@gmail.com>
2020-08-21 13:41:46 -0400

    Add function buttons to view window

--- ViewWin.cpp
+++ ViewWin.cpp
@@ -1,17 +1,20 @@
-#include "ViewWin.h"
 #include <FL/Fl_Double_Window.H>
+#include <FL/Fl_Button.H>
+
+#include "ViewWin.h"
 #include "Fl_Image_Display.H"
 
 Fl_Double_Window* _win;
 Fl_Image_Display* _disp;
 Pair* _viewing;
-bool _startLeft;
+bool _showLeft;
+Fl_Button* _bFull; // the 'Fit'/'100%' button
 
 void ViewImage()
 {
     const char* path;
     
-    if (_startLeft)
+    if (_showLeft)
         path = GetFD(_viewing->FileLeftDex)->Name->c_str();
     else
         path = GetFD(_viewing->FileRightDex)->Name->c_str();
@@ -21,12 +24,44 @@ void ViewImage()
     _disp->value(img);
 
     char buff[1024];
-    if (_startLeft)
+    if (_showLeft)
         sprintf(buff, "Left image: %s", path);
     else
         sprintf(buff, "Right image: %s", path);
 
     _win->label(buff);
+}
+
+void bSwapClick(Fl_Widget *, void *d)
+{
+    _showLeft = !_showLeft;
+    ViewImage();
+}
+
+void bFullClick(Fl_Widget *, void *d)
+{
+    // alternate between "100%" and "Fit"
+    bool full = _bFull->label()[0] == '1';
+    _bFull->label( full ? "Fit" : "100%" );
+    if (full)
+        _disp->scale(1.0);
+    else
+        _disp->scale(0);
+}
+
+void bZoomInClick(Fl_Widget *, void *)
+{
+    _disp->zoomDelta(-1);
+}
+
+void bZoomOutClick(Fl_Widget *, void *)
+{
+    _disp->zoomDelta(+1);
+}
+
+void bCloseClick(Fl_Widget *, void *)
+{
+    _win->hide();
 }
 
 void initShow()
@@ -44,10 +79,46 @@ void makeWin()
     _win = new Fl_Double_Window(x, y, w, h);
 
     _win->begin();
-    _disp = new Fl_Image_Display(0, 0, w, h);
+    _disp = new Fl_Image_Display(0, 0, w, h-27);
 
     // btnbar
     // swap, close, 100% / fit, zoom in, zoom out
+    {
+        Fl_Group* o = new Fl_Group(0, h-27, w, 27);
+        o->begin();
+        
+        int bx = 2;
+        int bw = 55;  // other buttons same width
+        int bwm = bw + 3; // margin
+        int bw2 = 80; // wider for zoom buttons
+        int bwm2 = bw2 + 3;
+        
+        Fl_Button* bSwap = new Fl_Button(bx, h-25, bw, 25);
+        bSwap->label("Swap");
+        bSwap->callback(bSwapClick);
+
+        bx += bwm;
+        _bFull = new Fl_Button(bx, h-25, bw, 25);
+        _bFull->label("100%");
+        _bFull->callback(bFullClick);
+
+        bx += bwm;
+        Fl_Button* bZoomIn = new Fl_Button(bx, h-25, bw2, 25);
+        bZoomIn->label("Zoom In");
+        bZoomIn->callback(bZoomInClick);
+
+        bx += bwm2;
+        Fl_Button* bZoomOut = new Fl_Button(bx, h-25, bw2, 25);
+        bZoomOut->label("Zoom Out");
+        bZoomOut->callback(bZoomOutClick);
+
+        bx += bwm2;
+        Fl_Button* bClose = new Fl_Button(bx, h-25, bw, 25);
+        bClose->label("Close");
+        bClose->callback(bCloseClick);
+        
+        o->end();
+    }
 
     _win->end();
     _win->resizable(_disp);
@@ -59,7 +130,7 @@ void showView(Pair* toview, bool startLeft)
         makeWin();
 
     _viewing = toview;
-    _startLeft = startLeft;
+    _showLeft = startLeft;
 
     _win->show();
     ViewImage(); // NOTE: title bar doesn't update until shown
