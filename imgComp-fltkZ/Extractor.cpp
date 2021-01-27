3b8d865c23a71c0fddd5d86b4f2ca16032deae09
fire-eggs <lifeattickville@gmail.com>
2021-01-27 13:50:02 -0500

    Fix problem extracting from archive with non-ANSI name

--- imgComp-fltkZ/Extractor.cpp
+++ imgComp-fltkZ/Extractor.cpp
@@ -45,11 +45,29 @@ void init()
 	extrRar = new BitExtractor(*lib7z, BitFormat::Rar);
 }
 
+#include <FL/fl_utf8.h> // for fl_utf8toUtf16()
+
+static wchar_t* utf8_to_wchar(const char* utf8, wchar_t*& wbuf, int lg = -1) 
+{
+	unsigned len = (lg >= 0) ? (unsigned)lg : (unsigned)strlen(utf8);
+	unsigned wn = fl_utf8toUtf16(utf8, len, NULL, 0) + 1; // Query length
+	wbuf = (wchar_t*)realloc(wbuf, sizeof(wchar_t) * wn);
+	wn = fl_utf8toUtf16(utf8, len, (unsigned short*)wbuf, wn); // Convert string
+	wbuf[wn] = 0;
+	return wbuf;
+}
+
+
 std::string _outpath;
 
 const char* ExtractFile(std::string arcpath, std::string filepath)
 {
 	init();
+
+	wchar_t* arcpathW = NULL;
+	wchar_t* filepathW = NULL;
+	utf8_to_wchar(arcpath.c_str(), arcpathW, -1);
+	utf8_to_wchar(filepath.c_str(), filepathW, -1);
 
 	std::wstring arcpath2(arcpath.begin(), arcpath.end());
 	std::wstring fpath2(filepath.begin(), filepath.end());
@@ -59,17 +77,20 @@ const char* ExtractFile(std::string arcpath, std::string filepath)
 	fs::path p(arcpath);
 	std::string outdir0(TEMPDIR2);
 	std::string outdir = outdir0 + p.stem().string() + std::string("\\");
-	std::wstring outdirW(outdir.begin(), outdir.end());
+
+	wchar_t* outdirW = NULL;
+	utf8_to_wchar(outdir.c_str(), outdirW, -1);
+	//std::wstring outdirW(outdir.begin(), outdir.end());
 
 	fs::create_directory(outdir);
 	try
 	{
 		if (hasEnding(arcpath, ".RAR") || hasEnding(arcpath, ".CBR"))
-			extrRar->extractMatching(arcpath2, fpath2, outdirW);
+			extrRar->extractMatching(arcpathW, filepathW, outdirW);
 		if (hasEnding(arcpath, ".ZIP") || hasEnding(arcpath, ".CBZ"))
-			extrZip->extractMatching(arcpath2, fpath2, outdirW);
+			extrZip->extractMatching(arcpathW, filepathW, outdirW);
 		if (hasEnding(arcpath, ".7Z") || hasEnding(arcpath, ".CB7"))
-			extr7z->extractMatching(arcpath2, fpath2, outdirW);
+			extr7z->extractMatching(arcpathW, filepathW, outdirW);
 
 		_outpath = (outdir + filepath);
 		return (_outpath).c_str();
